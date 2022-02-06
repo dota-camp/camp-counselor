cddde62833dee4b7e09503bae850b4f2e8dbb42e
Tyler Hampton <dr.frankinfurter@gmail.com>
2022-02-06 15:12:07 -0800

    General setup for Discord bot
    Adding Dockerfile and the config files necessary for Nostrum

---
+++ config/config.exs
@@ -0,0 +1,14 @@
+import Config
+
+config :nostrum,
+  token: System.get_env("DISCORD_TOKEN"),
+  num_shards: :auto
+
+config :counselor,
+  env: Mix.env()
+
+config :logger, :console,
+  format: "\n$time [$level] $levelpad $message $metadata\n",
+  metadata: [:interaction_data, :guild_id, :channel_id, :user_id]
+
+import_config "#{Mix.env()}.exs"
