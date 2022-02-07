44615b09eef53554deed13354823378bbb4957d8
Tyler Hampton <dr.frankinfurter@gmail.com>
2022-02-06 16:12:26 -0800

    Basic scaffolding complete
    Everything needed for communicating with Discord is complete. Tested with the /info command.

---
+++ lib/counselor_discord/consumer.ex
@@ -0,0 +1,31 @@
+defmodule CounselorDiscord.Consumer do
+  @moduledoc """
+  A Nostrum GenServer implementation that dispatches function calls according to incoming event type.
+  """
+  use Nostrum.Consumer
+
+  require Logger
+
+  alias CounselorDiscord.SlashCommands
+
+  def start_link do
+    Consumer.start_link(__MODULE__)
+  end
+
+  def handle_event({:READY, _data, _ws_state}) do
+    SlashCommands.register_commands()
+
+    version = to_string(Application.spec(:counselor, :vsn))
+    Nostrum.Api.update_status(:online, "on v#{version}")
+
+    Logger.info("Bot started!")
+  end
+
+  def handle_event({:INTERACTION_CREATE, interaction, _ws_state}) do
+    SlashCommands.handle_interaction(interaction)
+  end
+
+  def handle_event(_event) do
+    :noop
+  end
+end
